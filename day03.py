471fc6737527dd9c052b72df82efbb7d5330c8e3
Max Santos <santosemax>
2021-07-01 18:03:55 -0400

    Working on day 3

---
+++ day03.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+x, temp = 0, []
+
+with open("data/day03_example.txt", "r") as file:
+    reader = file.read()
+    reader_list = reader.splitlines()
+
+    i = 1
+    while
+    temp = reader_list[i][::3]
+    
+    print(temp)
+        
+
+
+# Stuff:
+#
+# Slope:
+#   Right Three and Down 1
+#   0.333333...
