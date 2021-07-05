a2084e34f668b67c1c64084d298c3335cbc7c3a8
Max Santos <maxedwardsantos@gmail.com>
2021-07-05 18:59:26 -0400

    Working on Day 5

--- day05.py
+++ day05.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 
-test = "FBFBBFFRLR"
+seat = "FBFBBFFRLR"
 
-rows = test[:7:]
+# Split first 7 chars and the rest
+#row = seat[:7] # 0 - 127
+#column = seat[-3:] # 0-7
+
+with open("data/day05_data.txt") as file:
+    data = file.read()
+    data = data.splitlines() 
+
+for line in data:
+    row = line[:7]
+    column = line[-3:]
+
+
+    # Finding row for current line
+    for alpha in line:
+        
+
