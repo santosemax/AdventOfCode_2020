944e73e9fc2fe820e8bd01eae19eb0e1260a1628
Max Santos <santosemax>
2021-07-05 21:45:28 -0400

    Completed Day 5

--- day05.py
+++ day05.py
@@ -1,21 +1,61 @@
 #!/usr/bin/python3
 
-seat = "FBFBBFFRLR"
 
-# Split first 7 chars and the rest
-#row = seat[:7] # 0 - 127
-#column = seat[-3:] # 0-7
+def findRow(passport):
+    lower, upper = 0, 127
+    for alpha in range(6):
+        mid = (lower + upper) // 2
+        if passport[alpha] == "F":
+            upper = mid
+        elif passport[alpha] == "B":
+            lower = mid + 1
 
+    if passport[6] == 'F':
+        return lower
+    else:
+        return upper
+
+def findColumn(passport):
+    lower, upper = 0, 7
+    for i in range(2):
+        mid = (lower + upper) // 2
+        if passport[i] == "L":
+            upper = mid
+        elif passport[i] == "R":
+            lower = mid + 1
+
+    if passport[2] == 'L':
+        return lower
+    else:
+        return upper
+        
+            
+largestID = 0
+ids = []
 with open("data/day05_data.txt") as file:
     data = file.read()
     data = data.splitlines() 
 
 for line in data:
-    row = line[:7]
-    column = line[-3:]
+    row = findRow(line[:7])
+    column = findColumn(line[7:])
+
+    id = row * 8 + column
+    ids.append(id)
+
+print(max(ids))
+
+for id in ids:
+    if id+1 not in ids and id+2 in ids:
+        missing = id + 1
+
+print(missing)
 
 
-    # Finding row for current line
-    for alpha in line:
-        
+
+
+
+
+
+
 
